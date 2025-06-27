ee92291e62183b598b5a411fcc381ff1ef67efbd
DanielMachado-web <danielmachadojhs@gmail.com>
2025-06-27 09:29:36 -0300

    alterando console javascript

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
 function generateNumber() {
 
-    console.log("me chamou estou aqui")
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
+
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    console.log(randomNumber);
+
+    console.log(min, max);
+
+    alert(`O número sorteado é: ${randomNumber}`);
+    
+
 }
 
 
