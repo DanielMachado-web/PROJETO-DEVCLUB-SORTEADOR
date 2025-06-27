b8a51b41b60fcec9b0f43537f23dc6f446b215fc
DanielMachado-web <danielmachadojhs@gmail.com>
2025-06-27 10:21:00 -0300

    alteração e inclusao arquivo js

---
+++ operadores artimeticos.js
@@ -0,0 +1,33 @@
+/* operadores artimeticos
+
+
++ -> adição
+- -> subtração
+* -> multiplicação
+/ -> divisão
+% -> resto da divisão
+++ -> incremento
+-- -> decremento
+Exemplo:
+let a = 10;
+let b = 5;
+let soma = a + b; // 15
+let subtracao = a - b; // 5
+let multiplicacao = a * b; // 50
+let divisao = a / b; // 2
+let resto = a % b; // 0
+a++; // 11
+a--;
+// 10
+b++; // 6
+b--; // 5
+ */
+
+
+const result=2
+
+console.log(result + 2); // Adição
+console.log(result - 2); // Subtração   
+
+console.log(result * 2); // Multiplicação
+console.log(result / 2); // Divisão
