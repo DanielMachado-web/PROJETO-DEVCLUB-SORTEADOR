4e6fe2525e7c45acc65f40ac226614015116d806
DanielMachado-web <danielmachadojhs@gmail.com>
2025-06-28 01:03:20 -0300

    alteraçao  javascript

--- SetTimeout e Interval.js
+++ SetTimeout e Interval.js
@@ -13,6 +13,8 @@
 */
 const delay = 2000; // 2 segundos
 
+
+
 setTimeout(() => {
     console.log("Executado após 2 segundos");
 }, delay);
@@ -35,3 +37,27 @@ setInterval(() => {
 // Para parar o setTimeout, você pode usar clearTimeout
 // clearTimeout(timeoutId);
 
+
+
+<html lang="pt-br">
+<body>
+    <h1>0</h1>
+    <button onclick="start()">Iniciar</button>
+    <button onclick="pause()">Pausar</button>
+</body>
+<script src="scripts.js"></script>
+</html>
+
+let number = 0;
+let cron
+let h1 = document.querySelector("h1");
+function start() {
+    cron = setInterval(() => {
+        number++;
+        h1.innerHTML = number;
+    }, 1000);
+}
+
+function pause() {
+    clearInterval(cron);
+}
